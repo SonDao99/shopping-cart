816fd15ff59be89568d92a2f3b7d0978f724464d
Son Dao <sondao1999@gmail.com>
2022-03-22 12:41:45 +0700

    Create shopping cart with CartItem as children. ShoppingCart displays total amount if cart is not empty.

---
+++ src/components/ShoppingCart.js
@@ -0,0 +1,46 @@
+import React from "react";
+import CartItem from "./CartItem";
+
+const ShoppingCart = (props) => {
+  const displayCart = () => {
+		return(
+			props.cart.map((cartItem) => {
+				return <CartItem 
+									key={cartItem.id}
+									cartItem={cartItem}
+									items={props.items}
+									setCart={props.setCart}
+									cart={props.cart}
+							 />
+			})
+		)
+	}
+
+	const calculateTotal = () => {
+		let total = 0;
+
+		for (let i = 0; i < props.cart.length; i += 1) {
+			const currentID = props.cart[i].id;
+			total += props.items[currentID].price * props.cart[i].amount;
+		}
+
+		return total.toFixed(2);
+	}
+
+	const displayTotal = () => {
+		if (props.cart.length === 0) {
+			return;
+		}
+
+		return(`Total: ${calculateTotal()} USD`);
+	}
+	
+	return(
+		<div className="shoppingCart">
+			{displayCart()}
+			{displayTotal()}
+		</div>
+	)
+}
+
+export default ShoppingCart;
